9444e7ea2f5b3b1f1a4038fc635730bc6d13de09
asdia0 <asdia.the.potato@gmail.com>
2021-05-25 14:28:08 +0800

    It would appear that my past self actually made it easy to transform this into a class library

--- src/Connect4/Program.cs
+++ src/Connect4/Program.cs
@@ -13,142 +13,6 @@
 
         public static Dictionary<string, int> Moves = new Dictionary<string, int>();
 
-        static void Main(string[] args)
-        {
-            nodeCount = 0;
-            Find(7, 6, 4);
-        }
-
-        public static void Play()
-        {
-            bool lengthDone = false, breadthDone = false, goFirstDone = false, toWinDone = false;
-            int length = 0, breadth = 0, goFirst = 0, toWin = 4;
-
-            while (!lengthDone)
-            {
-                Console.Write("Length of grid: ");
-                if (int.TryParse(Console.ReadLine(), out length))
-                {
-                    lengthDone = true;
-                    Console.Clear();
-                }
-            }
-
-
-            while (!breadthDone)
-            {
-                Console.Write("Breadth of grid: ");
-                if (int.TryParse(Console.ReadLine(), out breadth))
-                {
-                    breadthDone = true;
-                    Console.Clear();
-                }
-            }
-
-            while (!toWinDone)
-            {
-                Console.Write("Number of tokens in a row to win: ");
-                if (int.TryParse(Console.ReadLine(), out toWin))
-                {
-                    toWinDone = true;
-                    Console.Clear();
-                }
-            }
-
-
-            while (!goFirstDone)
-            {
-                Console.Write("Would you like to go first? (Y/N): ");
-                string response = Console.ReadLine();
-                if (response.ToLower() == "y")
-                {
-                    goFirst = 0;
-                    goFirstDone = true;
-                    Console.Clear();
-                }
-                else if (response.ToLower() == "n")
-                {
-                    goFirst = 1;
-                    goFirstDone = true;
-                    Console.Clear();
-                }
-            }
-
-            Game game = new Game(new Grid(length, breadth), 2, 4);
-
-            bool gameOn = true;
-
-            while (gameOn)
-            {
-                Console.WriteLine(game.Grid);
-                Console.WriteLine(game);
-
-                if (game.MoveList.Any())
-                {
-                    Console.WriteLine($"Computer: Column {game.MoveList[^1]}");
-                }
-
-                if (game.Turn == goFirst)
-                {
-                    bool moveDone = false;
-                    int column;
-
-                    while (!moveDone)
-                    {
-                        Console.Write("Column: ");
-                        if (int.TryParse(Console.ReadLine(), out column))
-                        {
-                            try
-                            {
-                                game.Play(column);
-                                Console.Clear();
-                                moveDone = true;
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    bool ok = false;
-                    while (!ok)
-                    {
-                        Random rnd = new Random();
-                        try
-                        {
-                            game.Play(rnd.Next(0, length));
-                            ok = true;
-                            Console.Clear();
-                        }
-                        catch
-                        {
-
-                        }
-                    }
-                }
-
-                if (game.Draw())
-                {
-                    Console.Clear();
-                    Console.WriteLine("Draw.");
-                    gameOn = false;
-                }
-                else
-                {
-                    int? winner = game.Winner();
-                    if (winner != null)
-                    {
-                        Console.Clear();
-                        Console.WriteLine($"Player {winner} won!");
-                        gameOn = false;
-                    }
-                }
-            }
-        }
-
         public static void SortColumnOrder(int length)
         {
             ColumnOrder = new int[length];
@@ -158,7 +22,7 @@
             }
         }
 
-        public static void Find(int length, int breadth, int toWin)
+        public static int FindBestColumn(int length, int breadth, int toWin)
         {
             Game g = new Game(new Grid(length, breadth), 2, toWin);
 
@@ -177,27 +41,19 @@
                 }
             }
 
-            int col;
-
             if (g.Turn % 2 == 0)
             {
-                col = firstMoves.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+                return firstMoves.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
             }
             else
             {
-                col = firstMoves.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                return firstMoves.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
             }
-
-            Console.WriteLine($"Column: {col}");
         }
 
         public static int NegaMax(Game game, int alpha, int beta, int depth)
         {
             // Credits to http://blog.gamesolver.org/solving-connect-four/04-alphabeta/
-
-            // TODO: Store to transposition table
-            // TODO: Load transposition table
-            // TODO: Load openings
 
             nodeCount++;
 
