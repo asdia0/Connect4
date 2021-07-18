94f31bfc39ef485325394cd76c88a3cd4ca4368e
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 20:26:37 +0800

    Add Move docs

--- src/Connect4/Move.cs
+++ src/Connect4/Move.cs
@@ -2,12 +2,26 @@
 {
     using System;
 
+    /// <summary>
+    /// Defines a move.
+    /// </summary>
     public struct Move
     {
+        /// <summary>
+        /// The coordinates of the token placed.
+        /// </summary>
         public (int X, int Y) Coordinates { get; set; }
 
+        /// <summary>
+        /// The player that made the move.
+        /// </summary>
         public int Player { get; set; }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Move"/> struct.
+        /// </summary>
+        /// <param name="grid">The grid where the move was made.</param>
+        /// <param name="token">The token that was moved.</param>
         public Move(Grid grid, Token token)
         {
             if (token.Player == null)
@@ -20,6 +34,10 @@
             this.Player = (int)token.Player;
         }
 
+        /// <summary>
+        /// Converts the move into a string.
+        /// </summary>
+        /// <returns>A string version of the x-coordinate of <see cref="Coordinates"/>.</returns>
         public override string ToString()
         {
             return this.Coordinates.X.ToString();
