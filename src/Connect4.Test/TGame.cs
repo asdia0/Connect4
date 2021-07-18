936783fdab7e8064761b42847a7775e798f84f32
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 14:54:01 +0800

    Removed new lines

--- src/Connect4.Test/TGame.cs
+++ src/Connect4.Test/TGame.cs
@@ -13,27 +13,16 @@ namespace Connect4.Test
             Game game = new(grid, 2, 2);
 
             Assert.AreEqual(grid, game.Grid);
-
             Assert.AreEqual(2, game.ToWin);
-
             Assert.AreEqual(2, game.Players);
-
             Assert.AreEqual(0, game.MoveList.Count);
-
             Assert.AreEqual(0, game.Turn);
-
             game.Play(0);
-
             Assert.AreEqual(1, game.Turn);
-
             game.Play(1);
-
             Assert.AreEqual(0, game.Turn);
-
             Assert.AreEqual(2, game.MoveList.Count);
-
             game.Play(0);
-
             Assert.AreEqual(0, game.Winner);
         }
 
@@ -47,23 +36,14 @@ namespace Connect4.Test
             Assert.AreEqual(grid.Length, game.Grid.Length);
             Assert.AreEqual(grid.Breadth, game.Grid.Breadth);
             Assert.AreEqual(grid.Tokens.Length, game.Grid.Tokens.Length);
-
             Assert.AreEqual(2, game.ToWin);
-
             Assert.AreEqual(2, game.Players);
-
             Assert.AreEqual(0, game.MoveList.Count);
-
             Assert.AreEqual(0, game.Turn);
-
             game.Play(0);
-
             Assert.AreEqual(1, game.Turn);
-
             game.Play(1);
-
             Assert.AreEqual(0, game.Turn);
-
             Assert.AreEqual(2, game.MoveList.Count);
         }
 
@@ -78,19 +58,12 @@ namespace Connect4.Test
             Assert.AreEqual(grid.Length, game.Grid.Length);
             Assert.AreEqual(grid.Breadth, game.Grid.Breadth);
             Assert.AreEqual(grid.Tokens.Length, game.Grid.Tokens.Length);
-
             Assert.AreEqual(2, game.ToWin);
-
             Assert.AreEqual(2, game.Players);
-
             Assert.AreEqual(1, game.MoveList.Count);
-
             Assert.AreEqual(1, game.Turn);
-
             game.Play(1);
-
             Assert.AreEqual(0, game.Turn);
-
             Assert.AreEqual(2, game.MoveList.Count);
         }
 
