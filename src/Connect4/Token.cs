01e62a7fbf02f4790072cf6d6cd92cf230578888
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 20:21:51 +0800

    Add Token docs

--- src/Connect4/Token.cs
+++ src/Connect4/Token.cs
@@ -1,17 +1,34 @@
 ﻿namespace Connect4
 {
+    /// <summary>
+    /// Defines a token.
+    /// </summary>
     public struct Token
     {
+        /// <summary>
+        /// Gets or sets the player that placed the token.
+        /// </summary>
         public int? Player { get; set; }
 
+        /// <summary>
+        /// Gets or sets the token's ID.
+        /// </summary>
         public int ID { get; set; }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Token"/> struct.
+        /// </summary>
+        /// <param name="id">The token's ID.</param>
         public Token(int id)
         {
             this.ID = id;
             this.Player = null;
         }
 
+        /// <summary>
+        /// Initiaises a new instance of the <see cref="Token"/> struct from another token.
+        /// </summary>
+        /// <param name="token">The token to clone.</param>
         public Token(Token token)
         {
             this.Player = token.Player;
