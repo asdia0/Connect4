2040c066e4f39fd084ebf586829699b676f5ae90
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 14:23:52 +0800

    Add TGame.ToWin()

--- src/Connect4.Test/TGame.cs
+++ src/Connect4.Test/TGame.cs
@@ -1,5 +1,6 @@
 namespace Connect4.Test
 {
+    using System.Collections.Generic;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -19,6 +20,14 @@ namespace Connect4.Test
             game.Play(1);
 
             Assert.AreEqual(0, game.Turn);
+        }
+
+        [TestMethod]
+        public void ToWin()
+        {
+            Game game = new(new(2, 2), 2, 2);
+
+            Assert.AreEqual(2, game.ToWin);
         }
     }
 }
