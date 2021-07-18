9378ba61aa2b928f5d067c002882908a3676be9c
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 16:22:32 +0800

    Add TMove

---
+++ src/Connect4.Test/TMove.cs
@@ -0,0 +1,20 @@
+﻿namespace Connect4.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TMove
+    {
+        [TestMethod]
+        public void Move()
+        {
+            int pid = 0;
+            Token t = new(0);
+            t.Player = pid;
+            Move move = new(new(2, 2), t);
+
+            Assert.AreEqual((0, 2), move.Coordinates);
+            Assert.AreEqual(pid, move.Player);
+        }
+    }
+}
