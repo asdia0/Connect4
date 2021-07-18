71421c36b955aa07b4e81eec4dc5806e5e014117
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 16:16:08 +0800

    Add TToken

---
+++ src/Connect4.Test/Token.cs
@@ -0,0 +1,33 @@
+﻿namespace Connect4.Test
+{
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TToken
+    {
+        [TestMethod]
+        public void TokenInt()
+        {
+            int id = 0;
+            Token t = new(id);
+
+            Assert.AreEqual(id, t.ID);
+            Assert.AreEqual(null, t.Player);
+        }
+
+        [TestMethod]
+        public void TokenToken()
+        {
+            int tid = 0;
+            int pid = 1;
+            Token clone = new Token(tid);
+            clone.Player = pid;
+
+            Token t = new(clone);
+
+            Assert.AreEqual(tid, t.ID);
+            Assert.AreEqual(pid, t.Player);
+        }
+    }
+}
